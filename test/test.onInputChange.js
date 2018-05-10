aca668345ee56596c1e79f99c80c4b2de0b01dbf
Jack <baijinbo@126.com>
2018-05-10 17:01:52 +0800

    remove test

--- test/test.onInputChange.js
+++
@@ -1,8 +0,0 @@
-// mocha did not support IE9-
-var expect = chai.expect
-$("#test").after("<br/><br/><div>" + navigator.userAgent + "</div>")
-describe("native 'input' feature support", function () {
-    it("current browser does not support", function () {
-        expect($.onInputSupport).to.not.be.ok
-    })
-})
